fd13158cc9c39ee0028804e9ef720c3ccbc3f129
Jacques <jmalan1995@gmail.com>
2023-06-17 09:24:00 +0200

    Created bash script to generate a default main.cpp file

---
+++ generate_main.sh
@@ -0,0 +1,7 @@
+printf "#include<iostream>\n" > main.cpp
+printf "using namespace std;\n\n" >> main.cpp
+
+printf "\nint main() {\n" >> main.cpp
+printf "\tcout << \"Hello world!\" << endl;\n" >> main.cpp
+printf "\treturn 0;\n" >> main.cpp
+printf "}\n" >> main.cpp
