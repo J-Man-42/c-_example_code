77ed6fd01a49bc88c09465a87084e651a1604211
Jacques-Laptop <jmalan1995@gmail.com>
2023-06-20 07:59:44 +0200

    generate_main.sh now confirms if user wants to overwrite existing main.cpp

--- generate_main.sh
+++ generate_main.sh
@@ -1,7 +1,21 @@
-printf "#include<iostream>\n" > main.cpp
-printf "using namespace std;\n\n" >> main.cpp
+# Prevent accidental overwrite.
+if ls main.cpp 2>/dev/null; then
+	printf "Overwrite existing main.cpp?  (Y/[N])\n"
+	printf "> "
+	read answer
+	if [[ $answer == "N"* || $answer == "n"* ]]; then
+		exit 1
+	fi
+fi
 
-printf "\nint main() {\n" >> main.cpp
+
+# Create the standard main.cpp that prints Hello world.
+printf "Creating new main.cpp"
+
+printf "#include <iostream>\n" > main.cpp
+printf "using namespace std;\n" >> main.cpp
+printf "\n\n" >> main.cpp
+printf "int main() {\n" >> main.cpp
 printf "\tcout << \"Hello world!\" << endl;\n" >> main.cpp
 printf "\treturn 0;\n" >> main.cpp
 printf "}\n" >> main.cpp
