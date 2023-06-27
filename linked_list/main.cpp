4ded81bdbc4d856a0dff9955739aaa058e1c6eb0
Jacques-Laptop <jmalan1995@gmail.com>
2023-06-27 08:13:27 +0200

    Linked List now uses external array displaying function

--- linked_list/main.cpp
+++ linked_list/main.cpp
@@ -4,62 +4,12 @@
 #include <thread>
 #include <cmath>
 #include "linked_list.h"
+#include "../display_structure/display_structure.h"
 #include "../move_cursor/move_cursor.h"
 #include "../random/random.h"
 using namespace std;
 using namespace std::this_thread;	// sleep_for, sleep_until
 using namespace std::chrono;		// nanoseconds, system_clock, seconds.
-
-
-// Elegantly displays the content of the array.
-void displayArray(uint* array, const uint SIZE) {
-	uint width;
-
-	// Do not print array of size 0.
-	if (SIZE == 0) {
-		return;
-	}
-
-	// Display the top border.
-	cout << "┌─";
-	for (size_t i = 0; i < SIZE; i++) {
-		width = 1;
-		if (array[i] > 0) {
-			width += log10(array[i]);
-		}
-		for (size_t j = 0; j < width; j++) {
-			cout << "─";
-		}
-		if (i < SIZE-1) {
-			cout << "─┬─";
-		} else {
-			cout << "─┐\n";
-		}
-	}
-
-	// Iterate through all elements.
-	for (size_t i = 0; i < SIZE; i++) {
-		cout << "│ " << array[i] << " ";
-	}
-	cout << "│\n";
-
-	// Display the bottom border.
-	cout << "└─";
-	for (size_t i = 0; i < SIZE; i++) {
-		width = 1;
-		if (array[i] > 0) {
-			width += log10(array[i]);
-		}
-		for (size_t j = 0; j < width; j++) {
-			cout << "─";
-		}
-		if (i < SIZE-1) {
-			cout << "─┴─";
-		} else {
-			cout << "─┘\n";
-		}
-	}
-}
 
 
 
