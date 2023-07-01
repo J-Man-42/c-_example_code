a54be27b29d7882027cb293dd1caf024e6179309
Jacques <jmalan1995@gmail.com>
2023-07-01 18:44:08 +0200

    Added script that removes unwanted trailing whitespace

---
+++ fix_tabs.sh
@@ -0,0 +1,45 @@
+# Show the help dialogue.
+function showHelp {
+	echo "╔══════════════════════════════════════════════════════════╗"
+	echo "║ Usage: ./fix_spaces.sh [option]                          ║"
+	echo "║ optional arguments:                                      ║"
+	echo "║    -h:  Shows the help dialogue                          ║"
+	echo "║    -f:  Select the file to update                        ║"
+	echo "║                                                          ║"
+	echo "║ Opens the help dialogue if no arguments are provided.    ║"
+	echo "╚══════════════════════════════════════════════════════════╝"
+	exit 1
+}
+
+
+
+# The main operations of the script starts here.
+################################################################################
+
+# Organize the input parameters.
+while getopts "hf:" arg; do
+	case $arg in
+		h) help=true;;
+		f) file="${OPTARG}";;
+	esac
+done
+
+if ! [[ $file ]]; then
+	help=true
+fi
+
+
+# Show help dialogue.
+if [[ $help ]]; then
+	showHelp
+fi
+
+
+# Read file and correct trailing whitespace.
+if [[ $file ]]; then
+	readarray -t lines < $file
+	printf "" > $file
+	for (( i = 0; i < "${#lines[@]}"; i++ )); do
+		echo "$(sed -e 's/[[:space:]]*$//'<<<"${lines[i]}")" >> $file
+	done
+fi
