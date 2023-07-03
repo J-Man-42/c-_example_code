8161d8cbed189b852491c57e6c4d9aa9dd76d738
Jacques-Laptop <jmalan1995@gmail.com>
2023-07-04 00:02:19 +0200

    Updated testing tree in main.cpp for display_structure

--- display_structure/main.cpp
+++ display_structure/main.cpp
@@ -14,9 +14,9 @@ public:
 		this->left = nullptr;
 		this->right = nullptr;
 	}
-	Node(const int data, const uint height = 1) {
+	Node(const uint data) {
 		this->data = data;
-		this->height = height;
+		this->height = 1;
 		this->left = nullptr;
 		this->right = nullptr;
 	}
@@ -30,11 +30,47 @@ public:
 		this->left = nullptr;
 		this->right = nullptr;
 	}
-	int data;
+	uint data;
 	uint height;
 	Node* left;
 	Node* right;
 };
+
+
+// Recursively adjust the tree's height.
+void adjustHeight(Node* node) {
+
+	// Stopping condition.
+	if (!node) {
+		return;
+	}
+
+	// Traverse left and right child.
+	adjustHeight(node->left);
+	adjustHeight(node->right);
+
+	// Update height of current node.
+	uint left = (node->left ? node->left->height : 0);
+	uint right = (node->right ? node->right->height : 0);
+	node->height = 1 + max(left, right);
+}
+
+
+// Recursively delete the entire tree.
+void deleteTree(Node* node) {
+	
+	// Stopping condition.
+	if (!node) {
+		return;
+	}
+
+	// Traverse left and right child.
+	deleteTree(node->left);
+	deleteTree(node->right);
+
+	// Delete the current node.
+	delete node;
+}
 
 
 // Main testing function.
@@ -58,14 +94,21 @@ int main() {
 	cout << "\nHeap:" << endl;
 	displayHeap(array, SIZE);
 
+
 	// Build the tree.
-	Node* root = new Node(50, 4);
-	root->left = new Node(25, 3);
-	root->right = new Node(75, 3);
-	root->left->left = new Node(15, 2);
-	root->left->right = new Node(35, 2);
-	root->right->left = new Node(65, 2);
-	root->right->right = new Node(85, 2);
+	Node* root = new Node(50);
+
+	// Height 4.
+	root->left = new Node(25);
+	root->right = new Node(75);
+	
+	// Height 3.
+	root->left->left = new Node(15);
+	root->left->right = new Node(35);
+	root->right->left = new Node(65);
+	root->right->right = new Node(85);
+	
+	// Height 2.
 	root->left->left->left = new Node(10);
 	root->left->left->right = new Node(20);
 	root->left->right->left = new Node(30);
@@ -75,9 +118,33 @@ int main() {
 	root->right->right->left = new Node(80);
 	root->right->right->right = new Node(90);
 
+	// Height 1.
+	root->left->left->left->left = new Node(8);
+	root->left->left->left->right = new Node(12);
+	root->left->left->right->left = new Node(18);
+	root->left->left->right->right = new Node(22);
+	root->left->right->left->left = new Node(28);
+	root->left->right->left->right = new Node(32);
+	root->left->right->right->left = new Node(38);
+	root->left->right->right->right = new Node(42);
+	root->right->left->left->left = new Node(58);
+	root->right->left->left->right = new Node(62);
+	root->right->left->right->left = new Node(68);
+	root->right->left->right->right = new Node(72);
+	root->right->right->left->left = new Node(78);
+	root->right->right->left->right = new Node(82);
+	root->right->right->right->left = new Node(88);
+	root->right->right->right->right = new Node(92);
+
+	// Dynamically adjust tree heights.
+	adjustHeight(root);
+
 	// Display the tree.
 	cout << "\nTree:" << endl;
 	displayTree<Node>(root);
+
+	// Delete dynamic memory.
+	deleteTree(root);
 
 	return 0;
 }
